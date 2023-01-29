4177d8b1924bbe5cea737895b98891728618187c
Brittany Cowper <brittferren@gmail.com>
2023-01-29 15:37:17 -0600

    added new function file testing for evenness

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+var x = 100;
+
+for(var i = x; i <= 200; i++) {
+  if((i % 3 === 0) && (i % 4 === 0)) {
+    console.log("LoopyLighthouse");
+  } else if(i % 3 === 0) {
+    console.log("Loopy");
+  } else if(i % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
+}
